f56a8bbf30c8e95a5703b0e8f32678f4d13c9a9d
Bill Tran <bjtran1234@gmail.com>
2017-01-29 10:41:33 -0800

    add all files

---
+++ src/Components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return <div className='title'>React Universal App</div>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src'
+  ],
+
+  output: {
+    path:     './build',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test:   /\.scss?$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test:    /\.jsx?$/,
+        exclude: /node_modules/,
+        loader:  'babel-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ]
+      }
+    ],
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'React Universal Starter App'
+      })
+    ],
+
+    devtool: 'source-map'
+  }
+};
